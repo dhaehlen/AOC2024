1959495131832938d49005d38e5e3a8d9d700186
dhaehlen <dhaehlen@ucalgary.ca>
2024-12-08 00:20:02 -0700

    day 3 part 2 working

--- AOC2024v2/Program.cs
+++ AOC2024v2/Program.cs
@@ -5,4 +5,5 @@ using AOC2024v2;
 //Day1.RunDay1Part2();
 //Day2.RunDay2Part1();
 //Day2.RunDay2Part2();
-Day3.RunDay3Part1();
+//Day3.RunDay3Part1();
+Day3.RunDay3Part2();
--- AOC2024v2/day3/Day3.cs
+++ AOC2024v2/day3/Day3.cs
@@ -30,4 +30,40 @@ public static class Day3
         Console.WriteLine($"Matches Found: {results.Count} Total Value: {runningTotal}");
     }
 
+    public static void RunDay3Part2()
+    {
+        string pattern = @"don't\(\)|do\(\)|(mul)\(([0-9]+),([0-9]+)\)";
+        Regex rx = new(pattern);
+
+        using StreamReader file = new("./day3/input.txt");
+        string text = file.ReadToEnd();
+        MatchCollection matches = rx.Matches(text);
+
+        ulong runningTotal = 0;
+        bool executeState = true;
+        foreach(Match match in matches)
+        {
+            switch(match.Value)
+            {
+                case "do()":
+                    executeState = true;
+                    Console.WriteLine("EXECUTE ON");
+                    break;
+                case "don't()":
+                    executeState = false;
+                    Console.WriteLine("EXECUTE OFF");
+                    break;
+                default:
+                    if(executeState)
+                    {
+                        ulong first = ulong.Parse(match.Groups[2].Value);
+                        ulong second = ulong.Parse(match.Groups[3].Value);
+                        Console.WriteLine($"Number1: {first}, Number2: {second}");
+                        runningTotal += first * second;
+                    }
+                    break;
+            }
+        }
+        Console.WriteLine($"Matches Found: {matches.Count} Total Value: {runningTotal}");
+    }  
 }
